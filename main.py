a435ad36fc7d8502acd3d85f292bb95ff87c4651
Marcello de Paula Ferreira Costa <marcellopfcosta@gmail.com>
2017-05-21 21:14:04 -0300

    Added Tomazela's comments to main.py

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ from os import path
 import sys
 
 def main():
-
+    # Checking input arguments
     if not(len(sys.argv) == 2):
-        sys.exit("Wrong arguments!\n$ python3 main.py <input.txt>")
+        sys.exit("# ERROR: Invalid number of arguments!\n The arguments must follow the rule:\n   $ python3 main.py <input_state_machine.txt>\n If you want to get Graphviz visualization, you must follow the rule:\n   $ python3 main.py <input_state_machine.txt> true")
 
-    # If argc == 3, than a txt file name should have been provided as argv[2]
+    # If argc == 2, than a txt file name should have been provided as argv[2]
     filename = sys.argv[1]
 
+    # Checking if the file exists
     if not (path.exists(filename)):
-        sys.exit("{0} not exists".format(filename))
+        sys.exit(" # Error: {0} not exists".format(filename))
 
+    # Parsing input file, which contains the State Machine and the CTL expression that is going to be evaluated
     graphParser = GraphParser(filename)
     graph = graphParser.parse()
+    
+    # Parsing CTL expression that was found in the file
     ctlParser = CTLParser(filename)
     expressions = ctlParser.parse()
-
-    evaluator = Evaluator(expressions[0], graph)
-    tree = evaluator.evaluate()
 
     print(" ---------- \n| CTL Tree |\n ---------- ")
     print(tree.toString())
@@ -35,8 +36,5 @@ def main():
     for key in sorted(graph.nodes):
         print(graph.nodes[key])
 
-    graph.render()
-    tree.render()
-
 if __name__ == '__main__':
-    main()
+main()
